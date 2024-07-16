4ee873b0a6106d29ba7c634ad900b797100aee12
Zeteticus <mdotson888@gmail.com>
2024-07-16 18:52:46 -0400

    Removed min_gap_duration

--- src/subgenix/subtitle_generator.py
+++ src/subgenix/subtitle_generator.py
@@ -5,6 +5,9 @@ from .progress_manager import ProgressManager
 
 
 class SubtitleGenerator:
+    max_segment_duration = 5.0  # Maximum duration for a single subtitle
+    max_pause_duration = 2.0  # Maximum duration of a pause before creating a new sub
+
     def __init__(self, progress_manager: ProgressManager):
         self.progress_manager = progress_manager
         logger.info("SubtitleGenerator initialized")
@@ -23,34 +26,70 @@ class SubtitleGenerator:
             raise
 
 
-    def _group_words_into_segments(self, word_timestamps):
-        segments = []
-        current_segment = []
-        current_start_time = word_timestamps[0][0]
-
-        for start, end, word in word_timestamps:
-            current_segment.append(word)
-            if word.endswith('.') or word.endswith('?') or word.endswith('!'):
-                segment_text = ' '.join(current_segment)
-                segments.append((current_start_time, end, segment_text))
-                current_segment = []
-                current_start_time = end
+#    def _group_words_into_segments(self, word_timestamps):
+#        segments = []
+#        current_segment = []
+#        current_start_time = word_timestamps[0][0]
+#
+#        for start, end, word in word_timestamps:
+#            current_segment.append(word)
+#            if word.endswith('.') or word.endswith('?') or word.endswith('!'):
+#                segment_text = ' '.join(current_segment)
+#                segments.append((current_start_time, end, segment_text))
+#                current_segment = []
+#                current_start_time = end
 
     # Add any remaining words as the final segment
-        if current_segment:
-            segment_text = ' '.join(current_segment)
-            segments.append((current_start_time, word_timestamps[-1][1], segment_text))
-            min_gap_duration = 0.5      # Define the minimum gap duration
-            max_segment_duration = 5.0  # Maximum duration for a single subtitle
-        for i in range(len(segments) - 1):
-            start_time, end_time, text = segments[i]
-            next_start_time, _, _ = segments[i + 1]
+#        if current_segment:
+#            segment_text = ' '.join(current_segment)
+#            segments.append((current_start_time, word_timestamps[-1][1], segment_text))
+#        for i in range(len(segments) - 1):
+#            start_time, end_time, text = segments[i]
+#            next_start_time, _, _ = segments[i + 1]
+#
+#            if next_start_time - end_time < min_gap_duration:
+#                new_end_time = min(end_time + max_segment_duration, next_start_time)
+#                segments[i] = (start_time, new_end_time, text)
+    def _group_words_into_segments(self, word_timestamps: Sequence[Tuple[float, float, str]]) -> List[Tuple[float, float, str]]:
+            """Group words into subtitle segments based on timing constraints and sentence structure."""
+            segments = []
+            current_segment = []
+            current_start_time = word_timestamps[0][0]
+            last_end_time = word_timestamps[0][1]
 
-            if next_start_time - end_time < min_gap_duration:
-                new_end_time = min(end_time + max_segment_duration, next_start_time)
-                segments[i] = (start_time, new_end_time, text)
+            for i, (start, end, word) in enumerate(word_timestamps):
+                current_segment.append(word)
+                segment_duration = end - current_start_time
+                pause_duration = start - last_end_time
 
-        return segments
+           # Check if we need to start a new segment
+                if segment_duration >= self.MAX_SEGMENT_DURATION or pause_duration >= self.MAX_PAUSE_DURATION:
+                    # Try to find a better split point
+                    split_index = self._find_split_index(current_segment)
+                    if split_index > 0:
+                        # Split the segment
+                        first_part = current_segment[:split_index]
+                        second_part = current_segment[split_index:]
+                   
+                   # Add the first part as a segment
+                        segments.append((current_start_time, word_timestamps[i-len(second_part)][1], " ".join(first_part)))
+                   
+                   # Start a new segment with the second part
+                        current_segment = second_part
+                        current_start_time = word_timestamps[i-len(second_part)+1][0]
+                    else:
+                        # If no good split point, just add the current segment
+                        segments.append((current_start_time, end, " ".join(current_segment)))
+                        current_segment = []
+                        current_start_time = end
+
+                last_end_time = end
+
+            # Add the last segment
+            if current_segment:
+                segments.append((current_start_time, word_timestamps[-1][1], " ".join(current_segment)))
+
+            return segments
 
     async def _write_srt_file(self, segments: List[Tuple[float, float, str]], output_file: str):
         total_segments = len(segments)
